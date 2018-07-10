6302ba2acc6596a90386a79348baa2b860c22e0b
trx <bahdepa2@gmail.com>
2018-07-10 21:24:27 +0400

    - entities was implemented by using annotation Hibernate - equals and hashCode methods was implemented for comparison object

--- src/main/java/com/trx0eth7/projects/model/entity/Customer.java
+++ src/main/java/com/trx0eth7/projects/model/entity/Customer.java
@@ -1,5 +1,116 @@
 package com.trx0eth7.projects.model.entity;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "customers")
 public class Customer implements IEntity {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "fatherName")
+    private String fatherName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    public Customer() {
+
+    }
+
+    public Customer(String firstName, String lastName, String fatherName, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fatherName = fatherName;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", fatherName='" + fatherName + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Customer)) return false;
+
+        Customer customer = (Customer) o;
+        return Objects.equals(getId(), customer.getId()) &&
+                Objects.equals(getFirstName(), customer.getFirstName()) &&
+                Objects.equals(getLastName(), customer.getLastName()) &&
+                Objects.equals(getFatherName(), customer.getFatherName()) &&
+                Objects.equals(getPhone(), customer.getPhone());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getFirstName() != null ? getFirstName().hashCode() : 0);
+        result = 31 * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        result = 31 * result + (getFatherName() != null ? getFatherName().hashCode() : 0);
+        result = 31 * result + (getPhone() != null ? getPhone().hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/com/trx0eth7/projects/model/entity/Mechanic.java
+++ src/main/java/com/trx0eth7/projects/model/entity/Mechanic.java
@@ -1,4 +1,115 @@
 package com.trx0eth7.projects.model.entity;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "mechanics")
 public class Mechanic implements IEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(mappedBy = "mechanic")
+    private List<Order> orders = new ArrayList<>();
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "fatherName")
+    private String fatherName;
+
+    @Column(name = "hourlyPay")
+    private String hourlyPay;
+
+    public Mechanic() {
+    }
+
+    public Mechanic(String firstName, String lastName, String fatherName, String hourlyPay) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fatherName = fatherName;
+        this.hourlyPay = hourlyPay;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getHourlyPay() {
+        return hourlyPay;
+    }
+
+    public void setHourlyPay(String hourlyPay) {
+        this.hourlyPay = hourlyPay;
+    }
+
+    @Override
+    public String toString() {
+        return "Mechanic{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", fatherName='" + fatherName + '\'' +
+                ", hourlyPay='" + hourlyPay + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Mechanic)) return false;
+
+        Mechanic mechanic = (Mechanic) o;
+        return Objects.equals(getId(), mechanic.getId()) &&
+                Objects.equals(getFirstName(), mechanic.getFirstName()) &&
+                Objects.equals(getLastName(), mechanic.getLastName()) &&
+                Objects.equals(getFatherName(), mechanic.getFatherName()) &&
+                Objects.equals(getHourlyPay(), mechanic.getHourlyPay());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getFirstName() != null ? getFirstName().hashCode() : 0);
+        result = 31 * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        result = 31 * result + (getFatherName() != null ? getFatherName().hashCode() : 0);
+        result = 31 * result + (getHourlyPay() != null ? getHourlyPay().hashCode() : 0);
+        return result;
+    }
 }
