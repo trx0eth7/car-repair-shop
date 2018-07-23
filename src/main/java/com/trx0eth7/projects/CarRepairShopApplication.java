f22467eb7ba6b27270131cf391731e61dca1db2d
trx0eth7 <bahdepa2@gmail.com>
2018-07-23 08:43:08 +0400

    Add Spring Boot Application class

---
+++ src/main/java/com/trx0eth7/projects/CarRepairShopApplication.java
@@ -0,0 +1,11 @@
+package com.trx0eth7.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRepairShopApplication {
+    public static void main(String... args){
+        SpringApplication.run(CarRepairShopApplication.class, args);
+    }
+}
