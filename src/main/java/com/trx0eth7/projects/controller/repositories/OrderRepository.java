4921254d83bfc64ddf6ee1d631165d03ac9a7807
trx0eth7 <bahdepa2@gmail.com>
2018-07-23 08:48:23 +0400

    Create Spring Data JPA repositories (access data) Repositories: Customer, Mechanic, Order (order has addition access methods)

---
+++ src/main/java/com/trx0eth7/projects/controller/repositories/OrderRepository.java
@@ -0,0 +1,18 @@
+package com.trx0eth7.projects.controller.repositories;
+
+import com.trx0eth7.projects.model.OrderStatus;
+import com.trx0eth7.projects.model.entity.Customer;
+import com.trx0eth7.projects.model.entity.Mechanic;
+import com.trx0eth7.projects.model.entity.Order;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order,Long> {
+    @Transactional
+    List<Order> findByDescriptionLike(String description);
+    List<Order> findByOrderStatus(OrderStatus orderStatus);
+    List<Order> findByCustomer(Customer customer);
+    List<Order> findByMechanic(Mechanic mechanic);
+}
