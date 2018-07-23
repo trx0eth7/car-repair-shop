abc03565440292f5065f60ef2d40e61b043394d4
trx0eth7 <bahdepa2@gmail.com>
2018-07-23 09:02:22 +0400

    Change service according to the requirements of Spring Boot

--- src/main/java/com/trx0eth7/projects/view/services/WebService.java
+++ src/main/java/com/trx0eth7/projects/view/services/WebService.java
@@ -1,46 +1,17 @@
 package com.trx0eth7.projects.view.services;
 
 import com.trx0eth7.projects.controller.EntityController;
-import com.trx0eth7.projects.controller.dao.impl.CustomerDao;
-import com.trx0eth7.projects.controller.dao.impl.MechanicDao;
-import com.trx0eth7.projects.controller.dao.impl.OrderDao;
-import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class WebService {
 
-    private static WebService instance;
+    @Autowired
     private EntityController controller;
-
-    private WebService() {
-    }
-
-    private WebService(EntityController controller) {
-        this.controller = controller;
-        initDaoComponentsOfController(controller);
-    }
-
-    public static WebService getInstance() {
-        if (instance == null) {
-            instance = new WebService(new EntityController());
-        }
-        return instance;
-    }
-
-    private void initDaoComponentsOfController(EntityController controller) {
-        Session session = controller.getSession();
-
-        CustomerDao customerDao = new CustomerDao(session);
-        MechanicDao mechanicDao = new MechanicDao(session);
-        OrderDao orderDao = new OrderDao(session);
-
-        controller.setCustomerDao(customerDao);
-        controller.setMechanicDao(mechanicDao);
-        controller.setOrderDao(orderDao);
-    }
 
     public EntityController controller() {
         return controller;
     }
-
 
 }
