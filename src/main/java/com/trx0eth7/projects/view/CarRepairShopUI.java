3a6380345be90b5b351db260944386b72451a52c
trx0eth7 <bahdepa2@gmail.com>
2018-07-30 09:36:55 +0400

    Add content to Main window (Tabs: customers, mechanics, orders)

--- src/main/java/com/trx0eth7/projects/view/CarRepairShopUI.java
+++ src/main/java/com/trx0eth7/projects/view/CarRepairShopUI.java
@@ -1,11 +1,15 @@
 package com.trx0eth7.projects.view;
 
-import com.trx0eth7.projects.view.services.WebService;
+import com.trx0eth7.projects.view.components.tabs.CustomerTabContent;
+import com.trx0eth7.projects.view.components.tabs.MechanicTabContent;
+import com.trx0eth7.projects.view.components.tabs.OrderTabContent;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,10 +18,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CarRepairShopUI extends UI {
 
     @Autowired
-    private WebService service;
+    private CustomerTabContent customerTab;
+    @Autowired
+    private MechanicTabContent mechanicTab;
+    @Autowired
+    private OrderTabContent orderTab;
+
+    private TabSheet navigationTab = new TabSheet();
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(new Label("Test"));
+        addNavigationTabs();
+        addRootContent();
+    }
+
+    private void addRootContent() {
+        Panel border = new Panel(navigationTab);
+        border.setSizeFull();
+
+        VerticalLayout rootContent = new VerticalLayout();
+        rootContent.addComponent(border);
+        setContent(rootContent);
+    }
+
+    private void addNavigationTabs() {
+        navigationTab.setSizeFull();
+        navigationTab.addTab(customerTab, "Customers");
+        navigationTab.addTab(mechanicTab, "Mechanics");
+        navigationTab.addTab(orderTab, "Orders");
     }
 }
